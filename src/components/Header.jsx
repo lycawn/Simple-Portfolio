8434896e8a371b75e35bf3f64bf4c6f97230a226
Angelos Antoniades <anillusionist9050@gmail.com>
2021-11-24 23:41:21 +0200

    Version 6

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
 import React, {useState, useEffect} from "react";
+import Player from "./Player";
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 function Header(){
   const wrapperRef = React.useRef(null);
@@ -35,7 +36,7 @@ function Header(){
     <Link to="/Work" href="#">Work</Link>
     <Link to="/Contact" href="#">Contact</Link>
     <Link to="/Social" href="#">One Page Archive</Link>
-  
+    <Player />
   </nav>
   
 </details>
---
+++ src/components/Player.jsx
@@ -0,0 +1,35 @@
+import React, { useState, useEffect } from "react";
+import Zoom from 'react-reveal/Zoom';
+const useAudio = url => {
+  const [audio] = useState(new Audio("/sounds/A Keepsake from the Past.mp3"));
+  const [playing, setPlaying] = useState(false);
+
+  const toggle = () => setPlaying(!playing);
+
+  useEffect(() => {
+      playing ? audio.play() : audio.pause();
+    },
+    [playing]
+  );
+
+  useEffect(() => {
+    audio.addEventListener('ended', () => setPlaying(false));
+    return () => {
+      audio.removeEventListener('ended', () => setPlaying(false));
+    };
+  }, []);
+
+  return [playing, toggle];
+};
+
+const Player = ({ url }) => {
+  const [playing, toggle] = useAudio(url);
+
+  return (
+    <Zoom delay={100}>  <div>
+      <button className="play" onClick={toggle}>{playing ? "Pause" : "Play"}</button>
+    </div></Zoom>
+  );
+};
+
+export default Player;
