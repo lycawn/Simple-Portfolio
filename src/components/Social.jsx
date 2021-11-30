dcf07d31ba43afc0a6fcffbc88ce830af997fa62
Angelos Antoniades <anillusionist9050@gmail.com>
2021-11-30 21:15:10 +0200

    Version 7.7

--- src/components/Social.jsx
+++ src/components/Social.jsx
@@ -81,7 +81,7 @@ return(
             </div>
             <a href="https://marsio-zabirko.herokuapp.com/" onClick="audio.play()" target="_blank" ><h2 className="" >React.Portfolio</h2><p>(under construction.)</p></a>
             <a href="https://advancedjs.herokuapp.com/" target="_blank" onClick="audio.play()" ><h2 className="" >ejs.blog</h2><p>(under construction.)</p></a>
-            <a href="https://movie-crate.herokuapp.com/" target="_blank" onClick="audio.play()" ><h2 className="" >react.movieWebsite</h2><p>(under construction.)</p></a>
+            <a href="https://movie-crate.herokuapp.com/" target="_blank" onClick="audio.play()" ><h2 className="" >react.movieWebsite</h2><p>(under construction.User Name : admin@moviecrate.com pass / admin123admin)</p></a>
             <hr></hr>
             </Bounce>
             </div>
--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -30,7 +30,7 @@ return (<div>
             </div> 
             <a href="https://marsio-zabirko.herokuapp.com/" target="_blank" onClick="audio.play()" ><h2 className="" >React.Portfolio</h2><p>(under construction.)</p></a>
             <a href="https://advancedjs.herokuapp.com/" target="_blank" onClick="audio.play()" ><h2 className="" >ejs.blog</h2><p>(under construction.)</p></a>
-            <a href="https://movie-crate.herokuapp.com/" target="_blank" onClick="audio.play()" ><h2 className="" >react.movieWebsite</h2><p>(under construction.)</p></a>
+            <a href="https://movie-crate.herokuapp.com/" target="_blank" onClick="audio.play()" ><h2 className="" >react.movieWebsite</h2><p>(under construction.User Name : admin@moviecrate.com pass / admin123admin)</p></a>
             <hr></hr>
             </Bounce>
             <Link to="/Preloader4" onClick="audio.play()" href="#"><h2 className="nextPage" >Next Page</h2></Link>
